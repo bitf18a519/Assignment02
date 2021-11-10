ee26087fdc8542e1c7062fe2e59534365fef7e18
bitf18a519 <bitf18a519@pucit.edu.pk>
2021-11-10 17:04:25 +0500

    Intent added and set intetnt on button for activity jump

--- app/src/main/java/com/example/assignment02/MainActivity.java
+++ app/src/main/java/com/example/assignment02/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.assignment02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button quizt,learnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quizt=findViewById(R.id.quiz);
+
+        quizt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
+
+
 }
