7a5880c882f21533078fe0b3f9f94f64904bde36
bitf18a519 <bitf18a519@pucit.edu.pk>
2021-11-10 20:10:53 +0500

    Buttons added in new activity

--- app/src/main/java/com/example/assignment02/MainActivity5.java
+++ app/src/main/java/com/example/assignment02/MainActivity5.java
@@ -2,13 +2,20 @@ package com.example.assignment02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    Button b1,b2;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        String letter=getIntent().getStringExtra("key");
     }
 }
