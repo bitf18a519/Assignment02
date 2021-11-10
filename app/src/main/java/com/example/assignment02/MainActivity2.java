ca242e42ddc87b00327c807eebaff7bf74c8bab1
bitf18a519 <bitf18a519@pucit.edu.pk>
2021-11-10 17:13:19 +0500

    Array generated

--- app/src/main/java/com/example/assignment02/MainActivity2.java
+++ app/src/main/java/com/example/assignment02/MainActivity2.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
 
+
+    String []Arabic={"ا","ب","ث","ج","ح","خ","ت","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","ف","ق","ل","م","ن","ك","ه","و","ي","غ"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
