ac7937e3818d306f5b557ac7796bf5580e2f67b8
bitf18a519 <bitf18a519@pucit.edu.pk>
2021-11-10 17:19:23 +0500

    button names changed and random class added for random numbers

--- app/src/main/java/com/example/assignment02/MainActivity2.java
+++ app/src/main/java/com/example/assignment02/MainActivity2.java
@@ -3,15 +3,25 @@ package com.example.assignment02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
+    TextView text;
+    Random num =new Random();
     String []Arabic={"ا","ب","ث","ج","ح","خ","ت","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","ف","ق","ل","م","ن","ك","ه","و","ي","غ"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
+
+
+
     }
 }
